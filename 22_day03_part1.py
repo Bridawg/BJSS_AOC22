cb9a9f194efb9483d036366dca59747313c71964
Brian Sargent <Bridawg@users.noreply.github.com>
2022-12-03 22:40:39 +0000

    Tidied Up

--- 22_day03_part1.py
+++ 22_day03_part1.py
@@ -1,18 +1,17 @@
-# Initialise Array
+# Initialise Values
 myValues=[]
 score=0
 counter=0
 myVal=None
-# Read lines into the array
+valueList=["","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+# Datafiles
 inputFile='22_day03_input.txt'
 #inputFile='22_day03_input_test.txt'
-valueList=["","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 with open(inputFile) as f:
     for line in f:
-       # print(line.strip())
         myValues.append(line.strip())
-#print(len(myValues))
 
 # Step though array
 for x in myValues:
@@ -30,6 +29,5 @@ for x in myValues:
            # print(i)
             #print(valueList.index(i))
             score = score + valueList.index(i)
-
         
 print(str(score)+" value")
