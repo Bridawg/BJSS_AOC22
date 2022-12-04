4803bbe8fd6031a5e665885f2423d56f3220e830
Brian Sargent <Bridawg@users.noreply.github.com>
2022-12-04 20:20:05 +0000

    Day 4

---
+++ 22_day04_part1.py
@@ -0,0 +1,37 @@
+# Initialise Values
+myValues=[]
+counter=0
+
+# Datafiles
+inputFile='22_day04_input.txt'
+#inputFile='22_day04_input_test.txt'
+
+with open(inputFile) as f:
+    for line in f:
+        myValues.append(line.strip())
+
+# Step though array
+for x in myValues:
+   # print(x)
+    myPair = x.split(",")
+    #print (myPair)
+    elfOneRange = myPair[0].split("-")
+    elfTwoRange = myPair[1].split("-")
+    print(elfOneRange)
+    print(elfTwoRange)
+    elfOneRange[0] = int(elfOneRange[0])
+    elfOneRange[1] = int(elfOneRange[1])
+    elfTwoRange[0] = int(elfTwoRange[0])
+    elfTwoRange[1] = int(elfTwoRange[1])
+    if elfOneRange[0] == elfTwoRange[0] or elfOneRange[1] == elfTwoRange[1]:
+        counter = counter + 1
+        print("overlap detected")
+    elif elfOneRange[0] < elfTwoRange[0]:
+        if elfOneRange[1]> elfTwoRange[1]:
+            counter = counter + 1
+            print("overlap detected")
+    elif elfOneRange[0] > elfTwoRange[0]:
+         if elfOneRange[1]< elfTwoRange[1]:
+            counter = counter + 1
+            print("overlap detected")
+print(counter)
