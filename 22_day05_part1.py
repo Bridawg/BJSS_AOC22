39ddc9f034eaad7283d0a14f86ec1d834affe40c
Brian Sargent <Bridawg@users.noreply.github.com>
2022-12-06 21:29:06 +0000

    Day5 on Day 6

--- 22_day05_part1.py
+++ 22_day05_part1.py
@@ -1,80 +1,78 @@
 # Initialise Values
 myValues=[]
-myGrid=[]
+#myGrid=[]
 myHolder=[]
 
 counter=0
+counter2 = 0
+counter3 = 0
 doMap=1
 # Datafiles
 inputFile='22_day05_input.txt'
-inputFile='22_day05_input_test.txt'
+#inputFile='22_day05_input_test.txt'
 
 with open(inputFile) as f:
     for line in f:
         myValues.append(line)
         counter += 1
-print("A")
-print(counter)
-counter=0
 
 # Step though array
 for x in myValues:
     #print(x)
     if len(x)<2:
         doMap=0
-        #Generate Map
-        myHolder = myHolder.reverse() 
+        # Generate Map
+        myHolder.reverse() 
         print(myHolder)
-       # temp = int((len(myHolder[0])-3)/4)+1
-        for y in myHolder:
-            print(temp)
-        # temp = int((len(x)-3)/4)+1
+        myHolder.pop(0)
+        Dim1 = len(myHolder)
+        Dim2 = int((len(myHolder[0])-3)/4)+1        
+        print(str(Dim1)+"x"+str(Dim2))
+        myGrid = [[None]*Dim1 for _ in range(Dim2)]
+        print(myHolder)
+        print(myGrid)
+        for y in myHolder:#step through 
+            counter=0
+            counter2=0
+            myLen = len(y)
+            while counter < myLen:
+                print("incoming")
+                print(counter2)
+                print(counter3)
+                stripe = y[counter:counter+3]
+                #print(stripe)
+                
+                if "[" in stripe:
+                    myGrid[counter2][counter3] = stripe[1:2]
+                    print(stripe[1:2])
+                counter +=4
+                counter2 += 1
+            counter3 +=1
+
         
-        # print(temp)
-        # while y<=temp:
-        #     bob=x[0:3].strip("[]")
-        # bob=x[0:3].strip("[]")
-        # print(bob)
+        print(myGrid)
+        for x in myGrid:
+            while(None in x):
+                x.remove(None)
+        print(myGrid)
 
-        # bob=(x[4:7].strip("[]")
-        # print(bob)
-
-        # bob=x[8:10].strip("[]")
-        # print(bob)
     else:
         if doMap==1: #Hold map
             myHolder.append(x)
-            #print(x)
 
         else: # process map instruction
             bob = x.split()
-            moveCount = bob[1]
-            moveFrom = bob[3]
-            moveTo = bob[5]
+            moveCount = int(bob[1])
+            moveFrom = int(bob[3])-1
+            moveTo = int(bob[5])-1
             print(bob)
             print(str(moveCount)+" "+str(moveFrom)+" "+str(moveTo))
+            for z in range(moveCount):
+                temp = myGrid[moveFrom].pop()
+                myGrid[moveTo].append(temp)
 
-#print("break")
-
-#     myPair = x.split(",")
-#     #print (myPair)
-#     elfOneRange = myPair[0].split("-")
-#     elfTwoRange = myPair[1].split("-")
-#     print(elfOneRange)
-#     print(elfTwoRange)
-#     elfOneRange[0] = int(elfOneRange[0])
-#     elfOneRange[1] = int(elfOneRange[1])
-#     elfTwoRange[0] = int(elfTwoRange[0])
-#     elfTwoRange[1] = int(elfTwoRange[1])
-#     if elfOneRange[0] == elfTwoRange[0] or elfOneRange[1] == elfTwoRange[1]:
-#         counter = counter + 1
-#         print("overlap detected")
-#     elif elfOneRange[0] < elfTwoRange[0]:
-#         if elfOneRange[1]> elfTwoRange[1]:
-#             counter = counter + 1
-#             print("overlap detected")
-#     elif elfOneRange[0] > elfTwoRange[0]:
-#          if elfOneRange[1]< elfTwoRange[1]:
-#             counter = counter + 1
-#             print("overlap detected")
-# print(counter)
+            print (myGrid)
+myString="Answer is: "
+for i in myGrid:
+    myString += i.pop()
+print(myString)
